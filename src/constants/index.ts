3d52b809c487f10ea201bd0762e78849ba169718
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-10-03 21:10:00 +0300

    add types, constants and utility functions

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://picsum.photos';
+
+export const IMAGE_SIZE = { width: 300, height: 200 };
+export const PAGE_SIZE = 2;
+export const PAGE_LIMIT = 100;
+
+export const DECK_SIZE = 10;
+export const CARD_SHOW_TIME = 5;
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type ImageIpsum = {
+	id: string;
+	author: string;
+	download_url: string;
+	url: string;
+	height: number;
+	width: number;
+};
+
+export type Image = Pick<ImageIpsum, 'id' | 'author' | 'url'> & {
+	uniqueId: string;
+};
